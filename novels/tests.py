973d02cb52f538bfe63eb14c9ccaa2dfaa47649e
My name <me@example.com>
2021-05-29 00:08:20 -0700

    Basic view now returns minimal HTML

--- novels/tests.py
+++ novels/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from novels.views import home_page
 
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Heaven Daos</title>', html)
+        self.assertTrue(html.endswith('</html>'))
