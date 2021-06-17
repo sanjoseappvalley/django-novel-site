8d37e594ab169cbb6668a07dbdc2eb469e96c1b6
nickng <nicknguyen0506@gmail.com>
2021-06-17 00:29:35 -0700

    refactor views and templates for transitions between chapters

--- novels/views.py
+++ novels/views.py
@@ -1,7 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Novel, Chapter
-
+from django.core.paginator import Paginator
 
 def home_page(request):
     current_novel_list = Novel.objects.all()
@@ -16,4 +16,6 @@ def novel_page(request, slug):
 def chapter_page(request, slug1, slug2):
     novel = get_object_or_404(Novel, slug=slug1)
     chapter = get_object_or_404(Chapter, slug=slug2)
-    return render(request, 'chapter.html', {'novel': novel, 'chapter': chapter})
+    nextchap = novel.chapter_set.filter(id__gt=chapter.id).order_by('id').first()
+    prevchap = novel.chapter_set.filter(id__lt=chapter.id).order_by('id').last()
+    return render(request, 'chapter.html', {'novel': novel, 'chapter': chapter, 'nextchap': nextchap, 'prevchap': prevchap})
