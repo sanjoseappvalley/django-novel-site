83670b61792259c7c2063f5ed9a4dbee6dc4ffde
nickng <nicknguyen0506@gmail.com>
2021-07-13 00:16:30 -0700

    add www. for allowed host

--- worlddaos/settings.py
+++ worlddaos/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    ALLOWED_HOSTS = [os.environ['SITENAME'], 'www.' + [os.environ['SITENAME']]
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
