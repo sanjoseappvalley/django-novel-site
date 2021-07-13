3ebd2a082c8664e8212a963ceb532dd3a99585ad
nickng <nicknguyen0506@gmail.com>
2021-07-12 19:53:54 -0700

    set STATIC_ROOT in settings

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,12 +12,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
-        self.fno = Novel(novel_name="Legend of Chusen")
-        self.fno.save()
-        self.fno.chapter_set.create(chapter_name='Chapter 1: Qing Yun')
-        self.c2 = self.fno.chapter_set.create(chapter_name='Chapter 2: Confuse', content='chapter 2 story line')
-        self.sno = Novel(novel_name="Great Demon God")
-        self.sno.save()
+        # self.fno = Novel(novel_name="Legend of Chusen")
+        # self.fno.save()
+        # self.fno.chapter_set.create(chapter_name='Chapter 1: Qing Yun')
+        # self.c2 = self.fno.chapter_set.create(chapter_name='Chapter 2: Confuse', content='chapter 2 story line')
+        # self.sno = Novel(novel_name="Great Demon God")
+        # self.sno.save()
 
     def tearDown(self):
         self.browser.quit()
@@ -37,30 +37,30 @@ class NewVisitorTest(StaticLiveServerTestCase):
         #     any(novel.text == 'Legend of Chusen' for novel in novels),
         #     "There is no novel with such name!"
         # )
-        self.assertIn('Legend of Chusen', [novel.text for novel in novels])
+        self.assertIn('Demons Among Gods', [novel.text for novel in novels])
         # He clicks on the first novel
-        self.browser.find_element_by_link_text("Legend of Chusen").click()
+        self.browser.find_element_by_link_text('Demons Among Gods').click()
         time.sleep(2)
         # The page moves to the novel page, and he sees the title and a list of chapters
         novel_name = self.browser.find_element_by_tag_name('h1').text
-        self.assertEqual(novel_name, "Legend of Chusen")
+        self.assertEqual(novel_name, 'Demons Among Gods')
         first_novel_url = self.browser.current_url
         # the url has the novel name with - at the end
         self.assertIn("legend-of-chusen", first_novel_url)
         chapters = self.browser.find_elements_by_tag_name('li')
         self.assertTrue(
-            any(chapter.text == 'Chapter 1: Qing Yun' for chapter in chapters),
+            any(chapter.text == 'Chapter 1: Ancient Tales' for chapter in chapters),
             "No chapter name matches!"
         )
         # he clicks on chapter 1 and the page changes to chapter 1 content page
         self.browser.find_element_by_partial_link_text("Chapter 1:").click()
         time.sleep(2)
         # He sees chapter 1 content and starts reading
-        self.assertEqual(self.browser.find_element_by_tag_name('h1').text, "Chapter 1: Qing Yun")
+        self.assertEqual(self.browser.find_element_by_tag_name('h1').text, "Chapter 1: Ancient Tales")
         # finishing chapter 1 he sees the next button, he clicks on it
         self.browser.find_element_by_link_text("Next Chapter").click()
         time.sleep(2)
-        self.assertEqual(self.browser.find_element_by_tag_name('h1').text, 'Chapter 2: Confuse')
+        self.assertEqual(self.browser.find_element_by_tag_name('h1').text, 'Chapter 2: Arena')
         # he's reading chapter 2
         self.assertIn(self.browser.find_element_by_tag_name('p').text, 'chapter 2 story line')
         # After reading chapter 2, Nick realizes that he wants to comment on the story, so he Register
